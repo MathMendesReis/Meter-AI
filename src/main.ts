56821b4b93f151be7079ae63ef0dece42fc59d72
Matheus Mendes <matheusmendesreis@outlook.com>
2024-08-30 20:54:11 -0300

    feat: update app main

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { env } from './infra/env/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
+  const config = new DocumentBuilder().build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(env.PORT);
 }
 bootstrap();
