9f97599b9b1f2d8b10fdd575f057991f6783b39b
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-03 10:23:50 -0300

    feat: create use list-measure-by-customer-id-use-case

---
+++ src/domain/measure/application/use-case/list-measure-by-customer-id-use-case.ts
@@ -0,0 +1,40 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { MeasurePrismaRepositorie } from 'src/infra/database/prisma/repositories/MeasurePrisma';
+@Injectable()
+export class ListMeasureByCustomerIdUseCase {
+  constructor(
+    private readonly measurePrismaRepositorie: MeasurePrismaRepositorie,
+  ) {}
+
+  async execute(id: string) {
+    const findAll = await this.measurePrismaRepositorie.findAll(id);
+    this.measureNotFound(findAll);
+    const transformedData = this.transformedData(findAll);
+    return {
+      customer_code: id,
+      measures: transformedData,
+    };
+  }
+
+  measureNotFound(array: any[]) {
+    if (array.length === 0) {
+      throw new HttpException(
+        {
+          error_code: 'MEASURES_NOT_FOUND',
+          error_description: 'Nenhuma leitura',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+  }
+
+  transformedData(array: any[]) {
+    return array.map((item) => ({
+      measure_uuid: item.id,
+      measure_datetime: item.measureDatetime,
+      measure_type: item.measureType,
+      has_confirmed: item.hasConfirmed,
+      image_url: item.image,
+    }));
+  }
+}
