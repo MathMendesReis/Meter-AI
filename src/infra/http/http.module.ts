02ad61bf338fa23024e7035b65852404bce7c378
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-03 10:24:44 -0300

    feat: add new service in module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
-import { UploadImageController } from './controller/uploadController';
 import { GeminiService } from 'src/domain/gemini/application/use-case/GeminiService';
 import { DatabaseModule } from '../database/prisma/database.module';
-import { PacthConfirmController } from './controller/PathConfirmController';
-import { ListById } from './controller/ListById';
+import { ListMeasureByCustomerIdController } from './controller/list-measure-by-customer-id-controller';
+import { ConfirmedValueMeasureController } from './controller/confirmed-valeu-measure-controller';
+import { UploadImageController } from './controller/upload-image-controller';
+import { ListMeasureByCustomerIdUseCase } from 'src/domain/measure/application/use-case/list-measure-by-customer-id-use-case';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [UploadImageController, PacthConfirmController, ListById],
-  providers: [GeminiService],
+  controllers: [
+    UploadImageController,
+    ConfirmedValueMeasureController,
+    ListMeasureByCustomerIdController,
+  ],
+  providers: [GeminiService, ListMeasureByCustomerIdUseCase],
 })
 export class HttpModule {}
