eb609e2790a62385af080dcebd6cd4ad233062e5
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-03 14:43:37 -0300

    feat: add IMeasurePrismaRepositorie in database module

--- src/infra/database/prisma/database.module.ts
+++ src/infra/database/prisma/database.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './PrismaService';
 import { MeasurePrismaRepositorie } from './repositories/MeasurePrisma';
+import { IMeasurePrismaRepositorie } from '../../../domain/measure/application/repositories/measure-repositorie';
 
 @Module({
-  providers: [PrismaService, MeasurePrismaRepositorie],
-  exports: [PrismaService, MeasurePrismaRepositorie],
+  providers: [
+    PrismaService,
+    { provide: IMeasurePrismaRepositorie, useClass: MeasurePrismaRepositorie },
+  ],
+  exports: [PrismaService, IMeasurePrismaRepositorie],
 })
 export class DatabaseModule {}
