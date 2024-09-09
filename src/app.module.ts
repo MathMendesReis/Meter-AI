5905d4e044eeeea03a39a0b077bbd26a4b94ca6b
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-09 08:36:22 -0300

    feat: add ServeStaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from './infra/http/http.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './infra/database/prisma/database.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'upload'),
     }),
     HttpModule,
     DatabaseModule,
