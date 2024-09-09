5fe8a32a2563c673a2c9ff62d8b882017bce6998
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-09 08:40:01 -0300

    feat: add image repositorie in database module

--- src/infra/database/prisma/database.module.ts
+++ src/infra/database/prisma/database.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './PrismaService';
 import { MeasurePrismaRepositorie } from './repositories/MeasurePrisma';
 import { IMeasurePrismaRepositorie } from '../../../domain/measure/application/repositories/measure-repositorie';
+import { ImageRepositorie } from 'src/domain/measure/application/repositories/image-repositorie';
+import { ImagePrisma } from './repositories/ImagePrisma';
 
 @Module({
   providers: [
     PrismaService,
     { provide: IMeasurePrismaRepositorie, useClass: MeasurePrismaRepositorie },
+    { provide: ImageRepositorie, useClass: ImagePrisma },
   ],
-  exports: [PrismaService, IMeasurePrismaRepositorie],
+  exports: [PrismaService, IMeasurePrismaRepositorie, ImageRepositorie],
 })
 export class DatabaseModule {}
