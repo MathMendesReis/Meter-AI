b381c27b7f93bf47ddcd650b82901404031d65e2
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-09 08:37:32 -0300

    feat: add new method: findAll

--- src/domain/measure/application/repositories/measure-repositorie.ts
+++ src/domain/measure/application/repositories/measure-repositorie.ts
@@ -3,11 +3,17 @@ import { MeasureModel } from 'src/domain/gemini/enterprise/MeasureModel';
 
 export abstract class IMeasurePrismaRepositorie {
   abstract createCustomer(body: MeasureModel): Promise<Measure>;
-  abstract findUnique(monthYear: string): Promise<Measure | null>;
+  abstract findUnique(
+    monthYear: string,
+    customerId: string,
+  ): Promise<Measure | null>;
   abstract findUniqueById(id: string): Promise<Measure | null>;
   abstract updateMeasureHasConfirmedToTrue(
     id: string,
     hasConfirmed: boolean,
   ): Promise<Measure>;
-  abstract findAll(customerId: string): Promise<Measure[]>;
+  abstract findAll(
+    customerId: string,
+    measure_type?: string,
+  ): Promise<Measure[]>;
 }
