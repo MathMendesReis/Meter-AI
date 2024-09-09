98d3d5ef2adc073fad94d781e96f1a41ca0c8f90
Matheus Mendes <matheusmendesreis@outlook.com>
2024-09-09 08:36:43 -0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,14 @@ import { env } from './infra/env/env';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const config = new DocumentBuilder().build();
+  const config = new DocumentBuilder()
+    .setTitle('Meter AI')
+    .setDescription(
+      'Serviço que gerencia a leitura individualizada de consumo de água e gás. Para facilitar a coleta da informação, o serviço utilizará IA para obter a medição através da foto de um medidor.',
+    )
+    .setVersion('1.0')
+    .addTag('Meter AI')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
